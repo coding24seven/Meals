41a214eb00c05c5544cb565e21aeeaf85189ea4a
AW <codingdayandnight@gmail.com>
2019-06-04 20:16:55 +0200

    sorted common webpack properties

--- webpack/webpack.public.common.js
+++ webpack/webpack.public.common.js
@@ -1,4 +1,42 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   entry: './public-source/entry.js',
+  output: {
+    path: path.resolve(process.cwd(), 'public'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // extract CSS from JS output file into CSS file
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer()
+        ]
+      }
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        { loader: MiniCssExtractPlugin.loader }, // 4. Convert the JS string into CSS files
+        {
+          loader: "css-loader", // 3. Turn CSS into a JS string
+          options: { url: false } // disable url handling in css...
+        },
+        { loader: "postcss-loader" }, // 2. for autoprefixer
+        {
+          loader: "sass-loader", // 1. Turn SASS into CSS through node-sass module
+          options: { outputStyle: 'expanded' }
+        }
+      ]
+    }]
+  }
 }
--- webpack/webpack.public.dev.js
+++ webpack/webpack.public.dev.js
@@ -1,9 +1,6 @@
-const webpack = require("webpack");
-const path = require("path");
 const common = require("./webpack.public.common");
 const merge = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require("autoprefixer");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: 'development',
@@ -17,40 +14,6 @@ module.exports = merge(common, {
     // publicPath: '/', // url path where compiled files are served
     writeToDisk: true,
     compress: false,
-  },
-  output: {
-    path: path.resolve(process.cwd(), 'public'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    // extract CSS from JS output file into CSS file
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [
-          autoprefixer()
-        ]
-      }
-    })
-  ],
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        { loader: MiniCssExtractPlugin.loader }, // 4. Convert the JS string into CSS files
-        {
-          loader: "css-loader", // 3. Turn CSS into a JS string
-          options: { url: false } // disable url handling in css...
-        },
-        { loader: "postcss-loader" }, // 2. for autoprefixer
-        {
-          loader: "sass-loader", // 1. Turn SASS into CSS through node-sass module
-          options: { outputStyle: 'expanded' }
-        }
-      ]
-    }]
-  }
+  } 
 }
 ) // merge ends
