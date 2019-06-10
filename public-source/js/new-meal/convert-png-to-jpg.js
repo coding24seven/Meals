d48e5268c3c6930af2f24879e055a205c96b3cea
AW <codingdayandnight@gmail.com>
2019-06-10 12:34:42 +0200

    colorCanvasBackground refactored

--- public-source/js/new-meal/convert-png-to-jpg.js
+++ public-source/js/new-meal/convert-png-to-jpg.js
@@ -23,33 +23,25 @@ export default function convertToJpg(uploadableImage, cbs) {
 
     uploadableImage.type = 'image/jpeg';
 
-    const canvas = document.createElement('canvas');
+    let canvas = document.createElement('canvas');
     canvas.width = image.width;
     canvas.height = image.height;
     canvas.getContext('2d').drawImage(image, 0, 0);
 
+    canvas = colorCanvasBackground(canvas, "white");
+
     // update the image object with new DataURL content
-    uploadableImage.setContentAsDataURL(canvasToImage(canvas, "white", uploadableImage));
+    uploadableImage.setContentAsDataURL(canvas.toDataURL(uploadableImage.type, 1.0));
 
     cbs[0](uploadableImage, newCBs);
   }
-  image.src = uploadableImage.getContentAsDataURL();
+  image.src = uploadableImage.getContentAsDataURL(uploadableImage.type, 1.0);
 }
 
-function canvasToImage(canvas, backgroundColor, uploadableImage) {
+function colorCanvasBackground(canvas, backgroundColor) {
 
   const context = canvas.getContext('2d');
   canvas = context.canvas;
-
-  // shortcut to height and width
-  const w = canvas.width;
-  const h = canvas.height;
-
-  // get the current pixel data for a specified portion of the canvas
-  const pixelData = context.getImageData(0, 0, w, h);
-
-  // store the current globalCompositeOperation
-  const compositeOperation = context.globalCompositeOperation;
 
   // set to draw behind current content
   context.globalCompositeOperation = "destination-over";
@@ -58,20 +50,7 @@ function canvasToImage(canvas, backgroundColor, uploadableImage) {
   context.fillStyle = backgroundColor;
 
   // draw background / rect on entire canvas
-  context.fillRect(0, 0, w, h);
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-  // get the image data from the canvas
-  const imageAsDataURL = canvas.toDataURL(uploadableImage.type, 1.0);
-
-  // clear the canvas
-  context.clearRect(0, 0, w, h);
-
-  // restore it with original / cached ImageData
-  context.putImageData(pixelData, 0, 0);
-
-  // reset the globalCompositeOperation to what it was
-  context.globalCompositeOperation = compositeOperation;
-
-  // Return the Base64 encoded data url string
-  return imageAsDataURL;
+  return canvas;
 }
