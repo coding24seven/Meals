fac5d70e8a295f8caf640a0557ec25db585fb925
Ubuntu <ubuntu@vps-72cad1ee.vps.ovh.net>
2026-05-01 13:04:23 +0000

    docker serve

--- docker-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --name meals -d --restart unless-stopped -v $(pwd):/app -p 43654:43654 meals /bin/bash
-
---
+++ docker-serve.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name meals -d --restart unless-stopped -v $(pwd):/app -p 43654:43654 meals npm run serve
+
