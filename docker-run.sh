a19efc24fa213dbc6b55b12e7f8eaf48d29b57c5
arek <codingdayandnight@gmail.com>
2026-05-01 13:39:03 +0200

    update docker run command

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD:/app -p 43654:43654 meals /bin/bash
-
-# PORT=43654 IP=0.0.0.0 node server/kuchnia-server.js
+docker run -d --restart unless-stopped -it -v $PWD:/app -p 43654:43654 meals /bin/bash
